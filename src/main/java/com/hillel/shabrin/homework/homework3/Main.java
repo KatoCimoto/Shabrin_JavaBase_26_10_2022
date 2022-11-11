685396788673e9aa12dbf999d97a6b411daad314
Shabrin Anton <dogue.13@yahoo.com>
2022-11-11 17:53:29 +0200

    all done v2

--- src/main/java/com/hillel/shabrin/homework/homework3/Main.java
+++ src/main/java/com/hillel/shabrin/homework/homework3/Main.java
@@ -2,7 +2,7 @@ package com.hillel.shabrin.homework.homework3;
 
 public class Main {
     public static void main(String[] args) {
-        String welcome = "Hello GitHUB!";
+        String welcome = "Hello GitHUB!!!";
         System.out.println(welcome);
     }
 }
